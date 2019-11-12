bd5398d462770c55dc3d755b18ed503c2f1a43f5
Leonard Sheng Sheng Lee <sheeeng@gmail.com>
2019-11-12 15:30:20 +0100

    fix: unique job name

--- seed-jobs/javaJobs.groovy
+++ seed-jobs/javaJobs.groovy
@@ -11,7 +11,7 @@ factory.createFolder(
 
 factory.createFolder(name: "java/acme")
 factory.createMultiBranchPipeline(
-    name: "java/acme",
+    name: "java/acme/jaunty-java",
     jobDescription: "Multibranch Pipeline Job for Java project of Acme team.",
     id: "3cd0df22-2692-465c-8ce8-c9a12ad6156a",
     gitUrl: "git@github.com:sheeeng/jaunty-java.git",
